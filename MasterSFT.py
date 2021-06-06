117a595eb467c657da08763aaac274d0a5985596
maxmarcus <chianghungtse@gmail.com>
2021-06-06 15:03:15 +0800

    主程式-異音

--- MasterSFT.py
+++ MasterSFT.py
@@ -83,25 +83,20 @@ class MainWindow(QtWidgets.QMainWindow):
         path = plotpath
         data.mkdir(path)
         del path
-
         if numberofchannel == 1:
             ch1SFT.ch1(fs, dt, N, fmin, fmax,tmax ,tmin ,wf , vm, dBvalve, plotres, TimeFrequency, dataharvest, complexdiag, Timedomain,
                        ch0instru, limtxset1, microphonechannel, filepath, plotpath,filetype)
-
         elif numberofchannel == 2:
             ch2SFT.ch2(fs, dt, N, fmin, fmax,tmax ,tmin ,wf , vm, dBvalve, plotres, TimeFrequency, dataharvest, complexdiag, Timedomain,
                        ch0instru, ch1instru, limtxset1, limtxset2, microphonechannel, filepath, plotpath,filetype)
-
         elif numberofchannel == 3:
             ch3SFT.ch3(fs, dt, N, fmin, fmax,tmax ,tmin ,wf , vm, dBvalve, plotres, TimeFrequency, dataharvest, complexdiag, Timedomain,
                        ch0instru, ch1instru, ch2instru, limtxset1, limtxset2, limtxset3, microphonechannel, filepath,
                        plotpath,filetype)
-
         elif numberofchannel == 4:
             ch4SFT.ch4(fs, dt, N, fmin, fmax,tmax ,tmin ,wf , vm, dBvalve, plotres, TimeFrequency, dataharvest, complexdiag, Timedomain,
                        ch0instru, ch1instru, ch2instru, ch3instru, limtxset1, limtxset2, limtxset3, limtxset4,
                        microphonechannel, filepath, plotpath,filetype)
-
         else:
             print('please check the number of input channel')
 
